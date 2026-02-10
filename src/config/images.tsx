b7010c98d18330ad472c312926486e0e33d26214
nicolassarmiento-entra <nickstark91@icloud.com>
2026-02-10 11:38:06 -0500

    Replace profile image JPG with JPEG
    Remove fotoPerfil.jpg, add fotoPerfil.jpeg, and update the import in src/config/images.tsx to reference the new .jpeg asset. Keeps the profile image filename/extension consistent across the project.

--- src/config/images.tsx
+++ src/config/images.tsx
@@ -4,7 +4,7 @@ import reacticon from "../assets/images/React.svg";
 import fluttericon from "../assets/images/fluttericon.svg";
 import djangoicon from "../assets/images/Django.svg";
 import fastapiicon from "../assets/images/FastAPI.svg";
-import fotoPerfil from "../assets/images/fotoPerfil.jpg";
+import fotoPerfil from "../assets/images/fotoPerfil.jpeg";
 import az104 from "../assets/images/AZ104.svg";
 import dp203 from "../assets/images/DP203.svg";
 import az305 from "../assets/images/AZ305.svg";
